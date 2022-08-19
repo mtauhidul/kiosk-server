84c1643a68f1c3668b14ccb64954eff39f9615cb
mtauhidul <mislam.tauhidul@gmail.com>
2022-08-20 04:04:35 +0600

    feature added and done

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ mongoose
 
 app.use(cors());
 app.use(express.static('build'));
-app.use(express.json());
+app.use(express.json({ limit: '25mb' }));
+app.use(express.urlencoded({ limit: '25mb' }));
 app.use(middleware.requestLogger);
 
 app.use('/api/patients', patientsRouter);
--- controllers/patients.js
+++ controllers/patients.js
@@ -7,12 +7,29 @@ patientsRouter.get('/', (request, response) => {
   });
 });
 
-patientsRouter.post('/', (request, response) => {
-  const patient = new Patient(request.body);
+patientsRouter.get('/:id', async (request, response, next) => {
+  try {
+    const patient = await Patient.findById(request.params.id);
+    if (patient) {
+      response.json(patient);
+    } else {
+      response.status(404).end();
+    }
+  } catch (exception) {
+    next(exception);
+  }
+});
 
-  patient.save().then((result) => {
-    response.status(201).json(result);
-  });
+patientsRouter.post('/', (request, response, next) => {
+  try {
+    const patient = new Patient(request.body);
+
+    patient.save().then((result) => {
+      response.status(201).json(result);
+    });
+  } catch (exception) {
+    next(exception);
+  }
 });
 
 module.exports = patientsRouter;
