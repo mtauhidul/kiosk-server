77aa4bcf60b23762c838fb84a113f71793ef1f21
mtauhidul <mislam.tauhidul@gmail.com>
2022-08-20 19:36:53 +0600

    timestamp added

--- models/patients.js
+++ models/patients.js
@@ -1,69 +1,72 @@
 const mongoose = require('mongoose');
 
-const patientSchema = new mongoose.Schema({
-  userInfo: {
-    fullName: String,
-    day: String,
-    month: String,
-    year: String,
-    location: String,
-  },
-  demographicsInfo: {
-    address: String,
-    address2: String,
-    city: String,
-    state: String,
-    zipcode: String,
-    phone: String,
-    email: String,
-    user: {
+const patientSchema = new mongoose.Schema(
+  {
+    userInfo: {
       fullName: String,
       day: String,
       month: String,
       year: String,
       location: String,
     },
+    demographicsInfo: {
+      address: String,
+      address2: String,
+      city: String,
+      state: String,
+      zipcode: String,
+      phone: String,
+      email: String,
+      user: {
+        fullName: String,
+        day: String,
+        month: String,
+        year: String,
+        location: String,
+      },
+    },
+    patientsPicture: String,
+    driversLicense: String,
+    primaryInsurance: {
+      insuranceName: String,
+      copayForSpecialist: String,
+      groupName: String,
+      groupString: String,
+      phoneString: String,
+    },
+    secondaryInsurance: {
+      insuranceName: String,
+      copayForSpecialist: String,
+      groupName: String,
+      groupString: String,
+      phoneString: String,
+    },
+    allergies: [String],
+    medications: [String],
+    familyHistory: {
+      diabetes: String,
+    },
+    medicalHistory: [String],
+    surgicalHistory: [String],
+    socialHistory: {
+      smoke: String,
+    },
+    shoeSize: {
+      shoeSize: String,
+    },
+    hippaPolicy: {
+      signature: String,
+    },
+    practicePolicies: {
+      signature: String,
+    },
+    survey: {
+      question: String,
+      answer: String,
+    },
   },
-  patientsPicture: String,
-  driversLicense: String,
-  primaryInsurance: {
-    insuranceName: String,
-    copayForSpecialist: String,
-    groupName: String,
-    groupString: String,
-    phoneString: String,
-  },
-  secondaryInsurance: {
-    insuranceName: String,
-    copayForSpecialist: String,
-    groupName: String,
-    groupString: String,
-    phoneString: String,
-  },
-  allergies: [String],
-  medications: [String],
-  familyHistory: {
-    diabetes: String,
-  },
-  medicalHistory: [String],
-  surgicalHistory: [String],
-  socialHistory: {
-    smoke: String,
-  },
-  shoeSize: {
-    shoeSize: String,
-  },
-  hippaPolicy: {
-    signature: String,
-  },
-  practicePolicies: {
-    signature: String,
-  },
-  survey: {
-    question: String,
-    answer: String,
-  },
-});
+  { timestamps: true }
+);
 
 patientSchema.set('toJSON', {
   transform: (document, returnedObject) => {
